f1f21dc149326bb325e9a7bf99dfb14acb14cdb6
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2024-11-12 14:21:09 +0100

    basic structure

--- blog_aplication/core/views.py
+++ blog_aplication/core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "core/home.html"
+
+
+class AboutView(TemplateView):
+    template_name = "core/about.html"
+
+
+class ContactView(TemplateView):
+    template_name = "core/contact.html"
