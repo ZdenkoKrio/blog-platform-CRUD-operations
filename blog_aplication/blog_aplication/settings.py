cac53aeccece7a52e7cba7a6d2d8a9aaa546db0b
Zdenko Čepan <37399415+ZdenkoKrio@users.noreply.github.com>
2024-11-18 13:23:39 +0100

    default profile img

--- blog_aplication/blog_aplication/settings.py
+++ blog_aplication/blog_aplication/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,7 +150,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
--- blog_aplication/users/models.py
+++ blog_aplication/users/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+def default_profile_picture():
+    return "profile_pics/default.png"
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pics/',
+        blank=True,
+        null=True,
+        default=default_profile_picture
+    )
     website = models.URLField(blank=True, null=True)
     twitter = models.URLField(blank=True, null=True)
     facebook = models.URLField(blank=True, null=True)
